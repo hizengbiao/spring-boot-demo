e2757494c04502a1288ef0f13c07aaeafb353624
hp-windows-zb-laptop <hizengbiao@qq.com>
2025-01-23 02:08:50 +0800

    添加tomcat测试模块

---
+++ a-demo-tomcat/src/main/java/com/tomcat/SpringBootACommonUtilsApplication.java
@@ -0,0 +1,23 @@
+package com.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-03-31 22:24
+ */
+@SpringBootApplication
+public class Tomcat {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tomcat.class, args);
+
+    }
+
+}
+
