f9b76c4b551e103babaf87e99b58a9222d5f6a3d
hp-windows-zb-laptop <hizengbiao@qq.com>
2025-04-14 01:55:32 +0800

    移除手动构造有参的连接池，启动时无数据库连接

--- a-demo-hikari/src/main/java/com/zb/hikari/controller/CallController.java
+++ a-demo-hikari/src/main/java/com/zb/hikari/controller/CallController.java
@@ -1,15 +1,13 @@
 package com.zb.hikari.controller;
 
 import com.zb.hikari.entity.UrlReqBody;
-import com.zb.hikari.entity.User;
-import com.zb.hikari.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/call")
